b01f60350b7368c57c2cbe4d3db86afa52125379
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-09 11:05:05 -0600

    added TryStuff class for experimenting

---
+++ src/TryStuff.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TryStuff {
+    public static void main(String[] args) {
+        guessAge();
+    }
+
+    public static void guessAge() {
+        Scanner in = new Scanner (System.in);
+        int guess = 0;
+        int age = 52;
+        System.out.print("Try to guess my age by entering a number. ");
+        while (age != guess) {
+            guess = in.nextInt();
+            if (guess < age) {
+                System.out.println("higher!");
+            }
+            if (guess > age) {
+                System.out.println("lower!");
+            }
+        }
+        System.out.println("You got it.");
+    }
+}
