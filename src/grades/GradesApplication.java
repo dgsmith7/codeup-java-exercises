d085e8aa6605995ad7e05c60f07ec1c9e4290dd9
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-11-15 12:21:21 -0600

    completed student grade app with bonuses

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -13,6 +13,7 @@ public class GradesApplication {
         boolean done = false;
         while (!done) {
             int sel = input.getInt(1, 5, displayMenu());
+            System.out.println();
             switch(sel) {
                 case 1: // Grades-attendance-absence report for 1 student
                     String student = getStudentName();
@@ -25,6 +26,7 @@ public class GradesApplication {
                     runClassAverageReport();
                     break;
                 case 4: // Output CSV summary(name,github,avg, attdc)
+                    outputCSV();
                     break;
                 default : // Exit
                     done = true;
@@ -35,16 +37,22 @@ public class GradesApplication {
 
     public static void outputCSV() {
         // (name,github,avg, attdc)
+        System.out.println("\nname,github_username,average");
+        for (String s : students.keySet()) {
+            pullSingleStudentRecord(s);
+            System.out.println(students.get(s).getName() + "," + s + "," + String.format("%.1f",students.get(s).getGradeAverage()));
+        }
+        System.out.println();
     }
 
     public static void runClassAverageReport() {
         // do the messaging and display piece then show average grades and attendance
         float sum = 0;
         for (String s : students.keySet()) {
-            runSingleStudentReport(s);
+ //           pullSingleStudentRecord(s);
             sum += students.get(s).getGradeAverage();
         }
-        double avg = sum / students.size();
+        System.out.println("Class average is " + String.format("%.1f", (sum / students.size())) +"\n");
     }
 
     public static void runSummaryReport() {
@@ -73,9 +81,9 @@ public class GradesApplication {
         reportItems[0] = students.get(_student).getName();
         reportItems[1] = _student;
         reportItems[2] = students.get(_student).getGrades().toString();
-        reportItems[3] = String.format("%.2f", students.get(_student).getGradeAverage());
-        reportItems[4] = "average attendance";  // todo
-        reportItems[5] = "the days student was absent";  // todo
+        reportItems[3] = String.format("%.1f", students.get(_student).getGradeAverage());
+        reportItems[4] = students.get(_student).getAttendanceAverage();
+        reportItems[5] = students.get(_student).getAbsenceList();
         return reportItems;
     }
 
@@ -98,7 +106,7 @@ public class GradesApplication {
         String menu = "Student Reports Menu:\n\n" +
                 "1 - Grades-attendance-absence report for 1 student\n" +
                 "2 - Summary report for all students\n" +
-                "3 - Class average and attendance\n" +
+                "3 - Class average\n" +
                 "4 - Output CSV summary\n" +
                 "5 - Exit\n\n" +
                 "Please enter you selection: ";
@@ -111,18 +119,38 @@ public class GradesApplication {
         s1.addGrade(95);
         s1.addGrade(100);
         s1.addGrade(90);
+        s1.recordAttendance("14 NOV 22", "P");
+        s1.recordAttendance("15 NOV 22", "P");
+        s1.recordAttendance("16 NOV 22", "P");
+        s1.recordAttendance("17 NOV 22", "P");
+        s1.recordAttendance("18 NOV 22", "P");
         Student s2 = new Student("Moe", emptyGrades );
         s2.addGrade(65);
         s2.addGrade(70);
         s2.addGrade(75);
+        s2.recordAttendance("14 NOV 22", "A");
+        s2.recordAttendance("15 NOV 22", "P");
+        s2.recordAttendance("16 NOV 22", "A");
+        s2.recordAttendance("17 NOV 22", "P");
+        s2.recordAttendance("18 NOV 22", "A");
         Student s3 = new Student("Curly", emptyGrades );
         s3.addGrade(93);
         s3.addGrade(87);
         s3.addGrade(85);
+        s3.recordAttendance("14 NOV 22", "P");
+        s3.recordAttendance("15 NOV 22", "P");
+        s3.recordAttendance("16 NOV 22", "P");
+        s3.recordAttendance("17 NOV 22", "P");
+        s3.recordAttendance("18 NOV 22", "A");
         Student s4 = new Student("Schemp", emptyGrades );
         s3.addGrade(55);
         s3.addGrade(65);
         s3.addGrade(75);
+        s4.recordAttendance("14 NOV 22", "A");
+        s4.recordAttendance("15 NOV 22", "P");
+        s4.recordAttendance("16 NOV 22", "P");
+        s4.recordAttendance("17 NOV 22", "P");
+        s4.recordAttendance("18 NOV 22", "A");
         students.put("smartStooge", s1);
         students.put("angryMoe", s2);
         students.put("heyMoe", s3);
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,14 +1,36 @@
 package grades;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Student {
     private String name;
     private ArrayList<Integer> grades = new ArrayList<>();
+    private HashMap<String, String> attendance = new HashMap<>();
 
     Student (String _name, ArrayList<Integer> _grades) {
         this.name = _name;
         this.grades = _grades;
+    }
+
+    public void recordAttendance(String date, String value) {
+        attendance.put(date, value);
+    }
+
+    public String getAttendanceAverage() {
+        int sum = 0;
+        for (String s : attendance.keySet())
+            if (attendance.get(s).equals("P")) sum++;
+        return sum + " out of " + attendance.size() + " = " + String.format("%.1f",((float)sum/(float)attendance.size() * 100.0)) + "%";
+    }
+
+
+    public String getAbsenceList() {
+        String list = "";
+        for (String s : attendance.keySet())
+            if (attendance.get(s).equals("A")) list += s + "   ";
+        if (list.equals("")) list += "none.";
+        return list;
     }
 
     // returns the student's name
